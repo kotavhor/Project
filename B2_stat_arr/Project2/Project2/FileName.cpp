f09711ea1acf9bc1c16ba29f00eec3fadfce5360
kotavhor <kotavhor@gmail.com>
2025-04-03 21:57:42 +0300

    Create new files

---
+++ B2_stat_arr/Project2/Project2/FileName.cpp
@@ -0,0 +1,150 @@
+#include<iostream>
+#include <algorithm>
+#include <ctime>
+#include <cstdlib>
+
+const int MAX_SIZE = 100;
+
+
+template <class T>
+T MultyEvenElemnts(T* arr, int32_t size);
+template <class T>
+T SumZerosElemnts(T* arr, int32_t size);
+
+template <class T>
+void Module(T* arr, int32_t size);
+
+
+template <class T>
+void InputArray(T* arr, int32_t size);
+
+int32_t InputSize(int32_t& size);
+
+template <class T>
+void InputRandomArray(T* arr, int32_t size);
+
+template <class T>
+void OutputtArray(T* arr, int32_t size);
+
+int main() {
+
+	setlocale(LC_ALL, "Russian");
+
+	int32_t size{};
+	double arr[MAX_SIZE];
+	int32_t choice{};
+	std::cout << "choise 1 sami vvodite 2 random" << '\n';
+	std::cin >> choice;
+	InputSize(size);
+
+	switch (choice)
+	{
+	case 1:
+		InputArray(arr, size);
+		break;
+
+	case 2:
+		InputRandomArray(arr, size);
+		break;
+
+	}
+
+	std::cout << "Произведение: " << MultyEvenElemnts(arr, size) << '\n';
+	std::cout << "Сумма элементов между нулями: " << SumZerosElemnts(arr, size) << '\n';
+	std::cout << "Вывод массива: " << std::endl;
+	Module(arr, size);
+	OutputtArray(arr, size);
+
+}
+
+template <class T>
+void InputArray(T* arr, int32_t size) {
+	std::cout << "Введите " << size << " элементов массива:" << std::endl;
+	for (int32_t i = 0; i < size; ++i) {
+		std::cout << "Элемент " << i + 1 << ": ";
+		std::cin >> arr[i];
+	}
+}
+
+
+int32_t InputSize(int32_t& size) {
+
+	std::cout << "Введите размер массива: ";
+	std::cin >> size;
+
+
+	return size;
+
+}
+template <class T>
+void OutputtArray(T* arr, int32_t size) {
+	for (size_t i = 0; i < size; i++)
+	{
+		std::cout << arr[i] << '\n';
+	}
+}
+
+template <class T>
+void InputRandomArray(T* arr, int32_t size) {
+	std::srand(static_cast<unsigned int>(std::time(nullptr)));
+
+	for (int32_t i = 0; i < size; ++i) {
+		arr[i] = std::rand();
+	}
+}
+
+
+template <class T>
+T MultyEvenElemnts(T* arr, int32_t size) {
+	T multy{ 1 };
+	for (size_t i = 1; i < size; i += 2)
+	{
+		multy *= arr[i];
+	}
+	return multy;
+}
+
+template <class T>
+T SumZerosElemnts(T* arr, int32_t size) {
+	T sum{ 0 };
+	int32_t FirstZeroIndex{ -1 };
+	int32_t SecondZeroIndex{ -1 };
+	for (size_t i = 0; i < size; i++)
+	{
+		if (arr[i] == 0) {
+			if (FirstZeroIndex == -1) {
+				FirstZeroIndex = i;
+			}
+			else
+			{
+				SecondZeroIndex = i;
+			}
+		}
+	}
+
+	if (SecondZeroIndex != -1) {
+
+		for (FirstZeroIndex++; FirstZeroIndex < SecondZeroIndex; FirstZeroIndex++)
+		{
+			sum += arr[FirstZeroIndex];
+		}
+
+	}
+	return sum;
+
+}
+
+template <class T>
+void Module(T* arr, int32_t size) {
+	int32_t new_size = size; 
+	for (int32_t i = 0; i < new_size;) {
+		if (std::abs(arr[i]) >= 1) {
+			std::swap(arr[i], arr[--new_size]); 
+			arr[new_size] = 0; 
+		}
+		else {
+			++i; 
+		}
+	}
+
+}
---
+++ Statick_input_arr_2/Project1/Project1/Function.h
@@ -0,0 +1,18 @@
+#include"Function.cpp"
+template <class T>
+T MultyEvenElemnts(T* arr, int32_t size);
+
+template <class T>
+T SumZerosElemnts(T* arr, int32_t size);
+
+template <class T>
+void Module(T* arr, int32_t& size);
+
+
+template <class T>
+void InputArray(T* arr, int32_t size);
+
+int32_t InputSize(int32_t& size);
+
+template <class T>
+void InputRandomArray(T* arr, int32_t size);
